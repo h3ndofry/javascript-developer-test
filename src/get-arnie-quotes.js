cc42e792705ff52709c3621b1cabbdd8e7f07024
h3ndofry <daniel.t.henderson@gmail.com>
2023-11-03 21:05:12 +1100

    adds solution to the problem

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * Gets a list of Arnie quotes.
+ * 
+ * @param {string[]} urls The list of URLs to retrieve
+ * @returns {Promise} The response containing an array of quotes
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(urls.map((url) => {
+    return httpGet(url).then((response) => {
+      return { [`${response.status === 200 ? 'Arnie Quote' : 'FAILURE'}`]: JSON.parse(response.body).message };    
+    })
+  })
+  );
 };
 
 module.exports = {
